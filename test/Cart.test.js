1c7c714c4a77b9164555ef1fd0411dce9efb1fbf
Atia-Khan <atia.khan@xloopdigital.com>
2023-04-13 06:45:16 +0500

    starts working

--- test/Cart.test.js
+++ test/Cart.test.js
@@ -2,8 +2,11 @@ const Cart = require('../src/Cart.js');
 const expect = require('chai').expect;
 
 describe('Cart', () => {
+    // AC 1: Given that I visit the site, when I begin shopping, then I expect my cart to be empty.
+
     it('should initialize as empty', () => {
-        const cart = new Cart()
+        //Arrange
+        const cart = new Cart();
 
         expect(cart.items).to.deep.equal([])
         expect(cart.totalPrice).to.be.equal(0)
