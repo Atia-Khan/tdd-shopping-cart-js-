0b6bad3eeff6212ede6da1f5d9174c4f9252dabf
Atia-Khan <atia.khan@xloopdigital.com>
2023-04-13 07:11:38 +0500

    AC 2 test case Failed.

--- test/Cart.test.js
+++ test/Cart.test.js
@@ -1,5 +1,6 @@
 const Cart = require('../src/Cart.js');
 const expect = require('chai').expect;
+const Items = require('../src/Item.js')
 
 describe('Cart', () => {
     // AC 1: Given that I visit the site, when I begin shopping, then I expect my cart to be empty.
@@ -10,5 +11,24 @@ describe('Cart', () => {
 
         expect(cart.items).to.deep.equal([])
         expect(cart.totalPrice).to.be.equal(0)
-    })
-});
+    });
+
+
+    // AC 2: Given I have an empty cart, when I add an Item, then I expect to see totalPrice reflect the sum of all the Items in my cart, times the quantities of each item.
+
+    it('should reflects the totalPrice of items according to their quantity', () => {
+        //Arrange
+        const cart = new Cart();
+        const itemNo1 = new Items('Louis Handbag', 500, true);
+
+        //Act
+        cart.addItems(itemNo1, 3);
+        const actual = 1500;
+
+        //Assert
+        
+        expect(cart.totalPrice).to.be.equal(actual);
+    });
+})
+
+
